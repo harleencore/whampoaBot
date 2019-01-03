aaad9fd848a0853e3912c0576f683d9237b39280
harleencore <harleen.k.sidhu@gmail.com>
2019-01-04 00:24:09 +0800

    elegantifying the solution: only fetch highest id each time

--- improv_generator/improv.py
+++ improv_generator/improv.py
@@ -19,8 +19,10 @@ def get_json_from_url(url):
     return js
 
 # fetches a list of updates-- messages sent to Bot
-def get_updates():
+def get_updates(offset=None):
     url = URL + "getUpdates"
+    if offset:
+        url += "?offset={}".format(offset)
     js = get_json_from_url(url) # get UPDATES from URL, parse into dictionary
     return js
 
@@ -41,17 +43,34 @@ def send_message(text, chat_id):
     url = URL + "sendMessage?text={}&chat_id={}".format(text, chat_id)
     get_url(url)
 
+# calculates highest ID of all the updates
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+# echo reply for each message that we receieve
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
 
 # this is so we can import our functions into another script
 # without running anything
 def main():
-    last_textchat = (None, None)
+    last_update_id = None
     while True:
-        text, chat = get_last_chat_id_and_text(get_updates())
-        if (text, chat) != last_textchat:
-            send_message(text, chat)
-            last_textchat = (text, chat)
-        time.sleep(0.5) 
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
 
 if __name__ == '__main__':
     main()
