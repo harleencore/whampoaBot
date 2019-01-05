bfb321457ee93bc7279efe561298b5990b2a8549
harleencore <harleen.k.sidhu@gmail.com>
2019-01-06 00:10:30 +0800

    adding dbhelper code

---
+++ whampoa/dbhelper.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+class DBHelper:
+
+    # just takes a db name and creates a db connection
+    def __init__(self, dbname="todo.sqlite"):
+        self.dbname = dbname
+        self.conn = sqlite3.connect(dbname)
+
+    # creates a new table called items in the db
+    # it has one column, description
+    def setup(self):
+        stmt = "CREATE TABLE IF NOT EXISTS items (description text)"
+        self.conn.execute(stmt)
+        self.conn.commit()
+
+    # takes the text for the item and inserts it into the db table
+    def add_item(self, item_text):
+        stmt = "INSERT INTO items (description) VALUES (?)"
+        args = (item_text, )
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    # takes the text for an item and removes it from the database
+    def delete_item(self, item_text):
+        stmt = "DELETE FROM items WHERE description = (?)"
+        args = (item_text, ) 
+        self.conn.execute(stmt, args)
+        self.conn.commit()
+
+    # returns a list of all items in our database
+    # use a list comprehension to take the first element of each item
+    # SQLite will always return data in tuple format, even when there is only
+    # one column
+    def get_items(self):
+        stmt = "SELECT description FROM items"
+        return [x[0] for x in self.conn.execute(stmt)]
