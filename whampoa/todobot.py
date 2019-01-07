41266407396726f658a694372db42e1b2a5406bc
harleencore <harleen.k.sidhu@gmail.com>
2019-01-07 23:29:51 +0800

    adding code to handle /start command, and to ignore all messages starting with '/' that are not '/done'

--- whampoa/todobot.py
+++ whampoa/todobot.py
@@ -74,6 +74,10 @@ def handle_updates(updates):
             if text == "/done":
                 keyboard = build_keyboard(items)
                 send_message("Select an item to delete", chat, keyboard)
+            elif text == "/start":
+                send_message("Welcome to your personal To Do list! Send any text to me and I'll store it as an item. Send /done to remove items", chat)
+            elif text.startswith("/"):
+                continue
             elif text in items:
                 db.delete_item(text, chat) # if item is duplicate, delete
                 items = db.get_items(chat) # update items variable
