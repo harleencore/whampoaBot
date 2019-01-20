812838dfe990c6b274b3044c0d2e2fb78836d014
harleencore <harleen.k.sidhu@gmail.com>
2019-01-20 15:28:01 +0800

    adding new file, whampoaBot- redoing all code (old: feedbackBOt was waay too messy). adding fully tested: add_child, /feedback code

--- whampoa/dbhelper.py
+++ whampoa/dbhelper.py
@@ -10,42 +10,63 @@ class DBHelper:
     # creates a new table called feedback in the db
     # it has  columns: observations, owner, children, subjects
     def setup(self):
-        tblstmt = "CREATE TABLE IF NOT EXISTS feedback (observations text, owner text, children text, subjects text)"
-        # adding indexing
-        feedbackidx = "CREATE INDEX IF NOT EXISTS feedbackIndex ON feedback (observations ASC)"
-        ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON feedback (owner ASC)"
-        self.conn.execute(tblstmt) # create the table
-        self.conn.execute(feedbackidx)
-        self.conn.execute(ownidx)
+        # tblstmt = "CREATE TABLE IF NOT EXISTS feedback (observations text, owner text, children text, subjects text)"
+        # # adding indexing
+        # feedbackidx = "CREATE INDEX IF NOT EXISTS feedbackIndex ON feedback (observations ASC)"
+        # ownidx = "CREATE INDEX IF NOT EXISTS ownIndex ON feedback (owner ASC)"
+        #
+        # self.conn.execute(tblstmt) # create the table
+        # self.conn.execute(feedbackidx)
+        # self.conn.execute(ownidx)
+        # self.conn.commit()
+
+        tblstmt_children = "CREATE TABLE IF NOT EXISTS children_table (children text, owner text)"
+        feedbackidx_children = "CREATE INDEX IF NOT EXISTS childrenIndex ON children_table (children ASC)"
+        ownidx_children = "CREATE INDEX IF NOT EXISTS ownIndex_children ON children_table (owner ASC)"
+
+        self.conn.execute(tblstmt_children)
+        self.conn.execute(feedbackidx_children)
+        self.conn.execute(ownidx_children)
         self.conn.commit()
 
-    # takes the text for the feedback and inserts it into the db table
-    def add_feedback(self, feedback_text, owner, children, subjects):
-        stmt = "INSERT INTO feedback (observations, owner, children, subjects) VALUES (?, ?)"
-        args = (feedback_text, owner, children, subjects)
-        self.conn.execute(stmt, args)
-        self.conn.commit()
-
-    # removing delete function temporarily
-    # # takes the text for an feedback and removes it from the database
-    # def delete_feedback(self, feedback_text, owner):
-    #     stmt = "DELETE FROM feedback WHERE observations = (?) AND owner = (?)"
-    #     args = (feedback_text, owner )
+    # # takes the text for the feedback and inserts it into the db table
+    # def add_feedback(self, feedback_text, owner, children, subjects):
+    #     stmt = "INSERT INTO feedback (observations, owner, children, subjects) VALUES (?, ?)"
+    #     args = (feedback_text, owner, children, subjects)
     #     self.conn.execute(stmt, args)
     #     self.conn.commit()
-
-    # check syntax
-    def get_feedback(self, owner, children, subjects):
-        stmt = "SELECT observations FROM feedback WHERE owner = (?) AND children = (?) AND subjects = (?)"
-        args = (owner, children, subjects, )
-        return [x[0] for x in self.conn.execute(stmt, args)]
+    #
+    # # removing delete function temporarily
+    # # # takes the text for an feedback and removes it from the database
+    # # def delete_feedback(self, feedback_text, owner):
+    # #     stmt = "DELETE FROM feedback WHERE observations = (?) AND owner = (?)"
+    # #     args = (feedback_text, owner )
+    # #     self.conn.execute(stmt, args)
+    # #     self.conn.commit()
+    #
+    # # check syntax
+    # def get_feedback(self, owner, children, subjects):
+    #     stmt = "SELECT observations FROM feedback WHERE owner = (?) AND children = (?) AND subjects = (?)"
+    #     args = (owner, children, subjects, )
+    #     return [x[0] for x in self.conn.execute(stmt, args)]
+    #
+    # def get_children(self, owner):
+    #     stmt = "SELECT children FROM feedback WHERE owner = (?)"
+    #     args = (owner, )
+    #     return [x[0] for x in self.conn.execute(stmt, args)]
+    #
+    # def get_subjects(self, owner, kid):
+    #     stmt = "SELECT subjects FROM feedback WHERE owner = (?) AND children = (?)"
+    #     args = (owner, )
+    #     return [x[0] for x in self.conn.execute(stmt, args)]
 
     def get_children(self, owner):
-        stmt = "SELECT children FROM feedback WHERE owner = (?)"
+        stmt = "SELECT children from children_table WHERE owner = (?)"
         args = (owner, )
         return [x[0] for x in self.conn.execute(stmt, args)]
 
-    def get_subjects(self, owner, kid):
-        stmt = "SELECT subjects FROM feedback WHERE owner = (?) AND children = (?)"
-        args = (owner, )
-        return [x[0] for x in self.conn.execute(stmt, args)]
+    def add_child(self, children, owner):
+        stmt = "INSERT INTO children_table(children, owner) VALUES (?, ?)"
+        args = (children, owner)
+        self.conn.execute(stmt, args)
+        self.conn.commit()
--- whampoa/whampoabot.py
+++ whampoa/whampoabot.py
@@ -8,6 +8,9 @@ db = DBHelper()
 TOKEN = "679424726:AAFhyVf602gZxaS0pEIfyiVwqOA7KASWbmw"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
 
+
+add = False
+
 # download content from URL and give us a string
 def get_url(url):
     response = requests.get(url)
@@ -17,7 +20,7 @@ def get_url(url):
 # get a string response, parses into dictionary
 def get_json_from_url(url):
     content = get_url(url) # get content from URL with get_url
-    js = json.loads(content) # parse into dictionary --> loads = load String
+    js = json.loads(content)
     return js
 
 # fetches a list of updates-- messages sent to Bot
@@ -25,7 +28,7 @@ def get_updates(offset=None):
     url = URL + "getUpdates?timeout=100" # with long polling
     if offset:
         url += "&offset={}".format(offset)
-    js = get_json_from_url(url) # get UPDATES from URL, parse into dictionary
+    js = get_json_from_url(url)
     return js
 
 # get chat ID and message text of most recent message
@@ -39,15 +42,11 @@ def get_last_chat_id_and_text(updates):
     chat_id = updates["result"][last_update]["message"]["chat"]["id"]
     return (text, chat_id)
 
-
 # takes text message and chat_id of intended recipient + sends msg
 def send_message(text, chat_id, reply_markup=None):
     text = urllib.parse.quote_plus(text)
     url = URL + "sendMessage?text={}&chat_id={}&parse_mode=Markdown".format(text, chat_id)
     if reply_markup:
-        # since we built the entire keyboard object in build_items_keyboard(),
-        # we can pass it along to telegram in this function whenever neccesary
-        # reply_markup has the keyboard ALONG WITH vals like one_time_keyboard = True
         url += "&reply_markup={}".format(reply_markup)
     get_url(url)
 
@@ -60,72 +59,33 @@ def get_last_update_id(updates):
     return max(update_ids)
 
 def handle_updates(updates):
-    give_feedback = False # by default, you're only viewing
-    child = ""
-    subject = ""
-    subjects = []
-    add_child = False
-    for update in updates["result"]: # loop through each update
+    global add
+    for update in updates["result"]:
         try:
-            # grab text and chat components
-            text = update["message"]["text"] # check message text
-            chat = update["message"]["chat"]["id"] # check user who sent msg
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
             children = db.get_children(chat)
-            if not add_child:
+            if not add:
                 if text == "/feedback":
                     keyboard = build_items_keyboard(children)
                     send_message("Select a child to submit feedback for", chat, keyboard)
-                    give_feedback = True
-                elif text == "/view":
-                    keyboard = build_items_keyboard(children)
-                    send_message("Select a child to view feedback for", chat, keyboard)
-                    give_feedback = False
-                elif text == "/addchild":
-                    send_message("Please enter the name of the child.", chat)
-                    add_child = True
-                elif text in children: # if a child was selected
-                    child = text # set current child
-                    subjects = db.get_subjects(chat, child)
-                    keyboard = build_items_keyboard(subjects)
-                    if give_feedback:
-                        send_message("Select a subject to submit feedback for", chat, keyboard)
-                    else:
-                        send_message("Select a subject to view feedback for", chat, keyboard)
-                elif text in subjects:
-                    subject = text
-                    if give_feedback:
-                        send_message("Adding " + subject +" feedback for " + child)
-                        add_feedback(text, chat, child, subject)
-                    else:
-                        send_message("Showing " + subject + " feedback for " + child)
-                        feedback = get_feedback(chat, child, subject)
-                        message = "\n".join(feedback)
-                        send_message(message, chat)
-
-                elif text == "/start":
-                    send_message("Welcome to the FeedbackBot! Send /feedback to submit feedback, or /view to pull up a list of past committed feedback.", chat)
-                elif text.startswith("/"):
-                    continue
+                elif text in children:
+                    send_message("Sending feedback for: " + text, chat)
+                if text == "/add_child":
+                    send_message("Please enter name", chat)
+                    add = True
             else:
-                db.add_child(chat, text)
-
-
-
-
+                db.add_child(text, chat)
+                add = False
         except KeyError:
             pass
+
 
 def build_items_keyboard(items):
     keyboard = [[item] for item in items]
     reply_markup = {"keyboard":keyboard, "one_time_keyboard": True}
     return json.dumps(reply_markup)
 
-
-
-
-
-# this is so we can import our functions into another script
-# without running anything
 def main():
     db.setup()
     last_update_id = None
